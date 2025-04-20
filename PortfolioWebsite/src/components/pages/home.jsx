555792366378e956c87dd08b88dc98ef388ccd15
Redemption882 <loquendohyper@gmail.com>
2025-04-20 17:20:55 +0200

    added animation when changing pages

--- PortfolioWebsite/src/components/pages/home.jsx
+++ PortfolioWebsite/src/components/pages/home.jsx
@@ -1,5 +1,6 @@
 import viteLogo from '/vite.svg'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { showCols } from "../portfolioMain.jsx";
 import { faEnvelope, faFileArrowDown } from "@fortawesome/free-solid-svg-icons"
 import { faLinkedin, faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons"
 import { Typewriter } from 'react-simple-typewriter'
@@ -9,6 +10,7 @@ import CurriculumVitae from "/media/files/curriculumVitae.pdf";
 import Picture from "/media/images/smusi.jpg"
 
 export default function showHomePage() {
+    const [ScreenSizeLg] = useState(window.innerWidth <= 991)
     const [ResizePhotoLg, setResizePhotoLg] = useState(window.innerWidth <= 991)
     const [ResizePhotoMobile, setResizePhotoMobile] = useState(window.innerWidth <= 539)
 
@@ -17,7 +19,9 @@ export default function showHomePage() {
             setResizePhotoLg(window.innerWidth <= 991)
             setResizePhotoMobile(window.innerWidth <= 539)
         };
-
+        if (window.innerWidth >= 991) {
+            showCols()
+        }
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
     }, []);
@@ -28,16 +32,16 @@ export default function showHomePage() {
     var IGAccount = 'https://www.instagram.com/capurroo.alessandro/'
     var CVFileName = 'Curriculum Vitae CAPURRO ALESSANDRO';
     return(
-        <div>
+        <div className='container'>
             <div className="row my-lg-0 my-5">
-                <div className="col-lg-5 d-flex justify-content-center mb-lg-0 mb-3">
+                <div id='leftCol' className={`col-lg-5 d-flex align-items-center justify-content-center mb-lg-0 mb-3 ${ScreenSizeLg ? "" : "left-col-hidden"}`}>
                     <img src={Picture} alt="Foto"  width={`${ResizePhotoLg ? `${ResizePhotoMobile ? "300" : "320"}` : "350"}`} 
                     height={`${ResizePhotoLg ? `${ResizePhotoMobile ? "300" : "320"}` : "350"}`} loading='lazy' 
                     className='rounded-circle border border-4 mb-lg-0 mb-5'/>
                 </div>
-                <div className="col-lg-7">
-                    <h1 className='fw-normal'><span className='fw-medium'>Ciao</span>, sono <span className='fw-bold fc-purple'>Alessandro</span>,</h1>
-                    <h2 className="fw-normal mb-3">Sono un <span className='fw-bold fc-teal'><Typewriter words={["Front-end", "Web"]} loop={0} cursor 
+                <div id='rightCol' className={`col-lg-7 ${ScreenSizeLg ? "" : "right-col-hidden"}`}>
+                    <h1 className='fw-normal'><span className='fw-medium'>Ciao</span>, sono <span className='fw-bold fc-teal'>Alessandro</span>,</h1>
+                    <h2 className="fw-normal mb-3">Sono un <span className='fw-bold fc-purple'><Typewriter words={["Front-end", "Web"]} loop={0} cursor 
                     cursorStyle='' typeSpeed={70} deleteSpeed={50} delaySpeed={1500}/></span><span className="fw-medium"> Developer.</span></h2>
                     <p className='fs-5 fw-normal mb-2'>Benvenuto nel mio portfolio online!</p> 
                     <p className='fs-5 fw-normal mb-1'> Qui puoi dare un’occhiata al mio percorso: studi, 
@@ -52,14 +56,14 @@ export default function showHomePage() {
                         <a type="button" className='btn btn-lg btn-outline-light social-btn rounded-circle me-2' href={LinkedinAccount} target="_blank">
                             <FontAwesomeIcon icon={faLinkedin} />
                         </a>
-                        <a type="button" className='btn btn-lg btn-outline-light social-btn rounded-circle me-2' href={GithubLink} target="_blank">
-                            <FontAwesomeIcon icon={faGithub} />
-                        </a>
                         <a type="button" className='btn btn-lg btn-outline-light social-btn rounded-circle me-2' href={IGAccount} target="_blank">
                             <FontAwesomeIcon icon={faInstagram} />
                         </a>
+                        <a type="button" className='btn btn-lg btn-outline-light social-btn rounded-circle me-2' href={GithubLink} target="_blank">
+                            <FontAwesomeIcon icon={faGithub} />
+                        </a>
                     </div>
-                    <a type='button' className='btn btn-lg px-4 text-center cv-btn btn-light rounded-5' 
+                    <a type='button' className='btn btn-lg px-4 fw-medium text-center cv-btn btn-light rounded-5' 
                         download={CVFileName} href={CurriculumVitae}> Scarica CV &nbsp;
                         <span><FontAwesomeIcon icon={faFileArrowDown}/></span>
                     </a>
--- PortfolioWebsite/src/components/portfolioMain.jsx
+++ PortfolioWebsite/src/components/portfolioMain.jsx
@@ -7,52 +7,79 @@ import AboutPage from './pages/about.jsx'
 import { useEffect, useState } from "react";
 
 export default function showPortfolioMain() {
-    const validPages = ["home", "about", "competenze", "educazione", "esperienza"];
+    const [ScreenSizeLg, setScreenSizeLg] = useState(window.innerWidth <= 991)
+
+    useEffect(() =>{
+        const handleResize = () => {
+            setScreenSizeLg(window.innerWidth <= 991)
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
     const scrollToTop = () => {
-        if (window.innerWidth <= 991) {
-            setTimeout(() => {
-                window.scrollTo({top: 50, behavior: "smooth" });
-            }, 350);
-        } else {
+        if (window.innerWidth >= 991) {
             window.scrollTo({top: 50, behavior: "smooth" });
+        } else {
+            window.scrollTo({top: 0, behavior: "instant" });
         }
-
     }
-    const blurPage = () => {
-        var content = document.getElementById("content")
-        content.classList.add("content-loading")
+
+    const hideCols = () => {
+        var leftCol = document.getElementById("leftCol")
+        var rightCol = document.getElementById("rightCol")
+        leftCol.classList.add("left-col-show")
+        rightCol.classList.add("right-col-show")
+        setTimeout(()=> {
+            leftCol.classList.remove("left-col-show")
+            rightCol.classList.remove("right-col-show")
+            leftCol.classList.add("left-col-hide")
+            rightCol.classList.add("right-col-hide")
+        }, 100)
         setTimeout(() => {
-            content.classList.remove("content-loading")
-            content.classList.add("content-loaded")
-        }, 100);
-        content.classList.remove("content-loaded")
-    };
-    
+            leftCol.classList.remove("left-col-hide")
+            rightCol.classList.remove("right-col-hide")
+        }, 1300);
+    }
+
     const closeNavbar = () => {
         if (window.innerWidth <= 991) {
             var navbarCollapse = document.querySelector(".navbar-toggler")
             navbarCollapse.click();
         }
     }
+    //////////////////////////
+    //Page navigation code
+    //////////////////////////
 
     const getCurrentPage = () => {
         const pathSegments = window.location.pathname.split("/");
         return pathSegments.length > 1 ? pathSegments[1] : "home";
     };
 
+    const validPages = ["home", "about", "competenze", "educazione", "esperienza"];
     const [subpage, setPage] = useState(null);
 
     const handleNavigation = (newPage, subpage = null) => {
         if (validPages.includes(newPage)) {
-            window.history.pushState(null, "", `/${newPage}`); 
-            setPage(newPage);
+            if (window.innerWidth <= 991) {
+                window.history.pushState(null, "", `/${newPage}`); 
+                setPage(newPage);
+            } else {
+                hideCols();
+                setTimeout(() => {
+                    window.history.pushState(null, "", `/${newPage}`); 
+                    setPage(newPage);
+                    showCols()
+                }, 1000);
+            }
+
         } else {
             window.history.replaceState(null, "", "/home");
             setPage("home");
         }
         scrollToTop();
-        blurPage();
         closeNavbar();
     };
 
@@ -72,9 +99,16 @@ export default function showPortfolioMain() {
                 window.history.replaceState(null, "", "/home");
                 setPage("home");
             } else {
-                setPage(currentPage);
+                if (window.innerWidth <= 991) {
+                    setPage(currentPage);
+                } else {
+                    hideCols();
+                    setTimeout(() => {
+                        setPage(currentPage);
+                        showCols()
+                    }, 1000);
+                }
             }
-            blurPage();
         };
             window.addEventListener("popstate", handlePopState);
         return () => {
@@ -92,31 +126,31 @@ export default function showPortfolioMain() {
 
     if (subpage === null) return null;
     return(
-        <div onLoad={() => blurPage()}>
+        <div onLoad={() => scrollToTop()}>
             <nav className="navbar navbar-expand-lg sticky-top p-4" data-bs-theme="dark">
                 <div className="container-lg">
                     <a className="navbar-brand" onClick={() => handleNavigation("home")}>Capurro Alessandro</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
                         data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" 
                         aria-expanded="false" aria-label="Toggle navigation">
-                        <span><FontAwesomeIcon className='fa-duotone fa-lg' icon={faBars}/></span>
+                        <span><FontAwesomeIcon className='fa-lg' icon={faBars}/></span>
                     </button>
                     <div className="collapse navbar-collapse d-lg-flex justify-content-md-end text-center" id="navbarCollapse">
                         <div className="navbar-nav mt-lg-0 mt-3">
-                            <a className={`nav-link mx-lg-1 ${subpage === "home" ? "active" : ""}`} onClick={() => handleNavigation("home")}>Home</a>
+                            <a className={`nav-link mx-lg-1 ${subpage === "home" ? `active ${ScreenSizeLg ? "" : "border-bottom border-light"}` : ""}`} onClick={() => handleNavigation("home")}>Home</a>
                             <div className="border-bottom d-lg-none d-block my-2"></div>
-                            <a className={`nav-link mx-lg-1 ${subpage === "about" ? "active" : ""}`} onClick={() => handleNavigation("about")}>About</a>
+                            <a className={`nav-link mx-lg-1 ${subpage === "about" ? `active ${ScreenSizeLg ? "" : "border-bottom border-light"}` : ""}`} onClick={() => handleNavigation("about")}>About</a>
                             <div className="border-bottom d-lg-none d-block my-2"></div>
-                            <a className={`nav-link mx-lg-1 ${subpage === "educazione" ? "active" : ""}`} onClick={() => handleNavigation("educazione")}>Educazione</a>
+                            <a className={`nav-link mx-lg-1 ${subpage === "educazione" ? `active ${ScreenSizeLg ? "" : "border-bottom border-light"}` : ""}`} onClick={() => handleNavigation("educazione")}>Educazione</a>
                             <div className="border-bottom d-lg-none d-block my-2"></div>
-                            <a className={`nav-link mx-lg-1 ${subpage === "competenze" ? "active" : ""}`} onClick={() => handleNavigation("competenze")}>Competenze</a>
+                            <a className={`nav-link mx-lg-1 ${subpage === "competenze" ? `active ${ScreenSizeLg ? "" : "border-bottom border-light"}` : ""}`} onClick={() => handleNavigation("competenze")}>Competenze</a>
                             <div className="border-bottom d-lg-none d-block my-2"></div>
-                            <a className={`nav-link mx-lg-1 ${subpage === "esperienza" ? "active" : ""}`} onClick={() => handleNavigation("esperienza")}>Esperienza</a>
+                            <a className={`nav-link mx-lg-1 ${subpage === "esperienza" ? `active ${ScreenSizeLg ? "" : "border-bottom border-light"}` : ""}`} onClick={() => handleNavigation("esperienza")}>Esperienza</a>
                         </div>
                     </div>
                 </div>
             </nav>
-            <div id='content' className={`container min-vh-100 ${subpage === "home" || subpage === "about" ? "d-flex align-items-center" : ""} `}  data-bs-theme="dark">
+            <div id='section' className={`container-fluid ${ScreenSizeLg ? "" : "min-vh-100"} ${subpage === "home" || subpage === "about" ? "d-flex align-items-center" : ""} `}  data-bs-theme="dark">
                 {subpage === "home" && <HomePage/>}
                 {subpage === "about" && <AboutPage/>}
                 {subpage === "competenze" && <TextPage/>}
@@ -129,3 +163,20 @@ export default function showPortfolioMain() {
         </div>
     )
 }
+
+export const showCols = () => {
+    var leftCol = document.getElementById("leftCol")
+    var rightCol = document.getElementById("rightCol")
+
+    setTimeout(()=> {
+        leftCol.classList.remove("left-col-hidden")
+        rightCol.classList.remove("right-col-hidden")
+        leftCol.classList.add("col-show-anim")
+        rightCol.classList.add("col-show-anim")
+    }, 100)
+    setTimeout(() => {
+        leftCol.classList.remove("col-show-anim")
+        rightCol.classList.remove("col-show-anim")
+    }, 1300);
+};
+
