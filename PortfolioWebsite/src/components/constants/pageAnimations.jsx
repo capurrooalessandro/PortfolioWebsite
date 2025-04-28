176290a686d3bc4634b81c2cc799e214252834ab
Redemption882 <loquendohyper@gmail.com>
2025-04-28 02:57:10 +0200

    removed hidecols and modified subpage value

--- PortfolioWebsite/src/components/constants/pageAnimations.jsx
+++ PortfolioWebsite/src/components/constants/pageAnimations.jsx
@@ -10,7 +10,7 @@ import { subpage } from "../portfolioMain.jsx"
 export const scrollToTop = () => { 
     // in base alla pagina viene eseguito uno scroll automatico 
     // che riporta la pagina nella posizione corretta
-    if (subpage === "educazione" && window.innerWidth >= 991 || subpage === "esperienze" && window.innerWidth >= 991) { 
+    if (subpage === "/educazione" && window.innerWidth >= 991 || subpage === "/esperienze" && window.innerWidth >= 991) { 
         // se la grandezza dello schermo è maggiore di 991px e la pagina è "educazione" o "esperienze" 
         // viene eseguito uno scroll automatico che riporta la pagina nella posizione corretta
         window.scrollTo({top: 25, behavior: "smooth" });
@@ -23,87 +23,6 @@ export const scrollToTop = () => {
 
 
 ///////////////////////////////////////////////////////////////////////////////
-/// Funzione per nascondere le colonne e il testo (utilizzata per le animazioni
-/// delle pagine)
-///////////////////////////////////////////////////////////////////////////////
-
-export const hideCols = () => { 
-    // se la pagina è "educazione" o "esperienze" viene eseguita una animazione
-    // diversa di chiusura delle colonne e del testo, altrimenti viene eseguita 
-    // una animazione di chiusura standard
-    if (subpage == "educazione") { 
-        var firstRow = document.getElementById("firstRow")
-        var secondRow = document.getElementById("secondRow")
-        var leftCol = document.getElementById("leftCol")
-        var rightCol = document.getElementById("rightCol")
-        var leftColGastaldi = document.getElementById("leftColGastaldi")
-        var rightColGastaldi = document.getElementById("rightColGastaldi")
-        leftCol.classList.add("left-col-show")
-        rightCol.classList.add("right-col-show")
-        leftColGastaldi.classList.add("left-col-show")
-        rightColGastaldi.classList.add("right-col-show")
-        setTimeout(()=> {
-            leftCol.classList.remove("left-col-show")
-            rightCol.classList.remove("right-col-show")
-            leftCol.classList.add("left-col-hide")
-            rightCol.classList.add("right-col-hide")
-            setTimeout(() => {
-                leftColGastaldi.classList.remove("left-col-show")
-                rightColGastaldi.classList.remove("right-col-show")
-                leftColGastaldi.classList.add("left-col-hide")
-                rightColGastaldi.classList.add("right-col-hide")
-                firstRow.classList.remove("bg-card")
-                secondRow.classList.remove("bg-card")
-            }, 100);
-        }, 100)
-
-        setTimeout(() => {
-            leftCol.classList.remove("left-col-hide")
-            rightCol.classList.remove("right-col-hide")
-            leftColGastaldi.classList.remove("left-col-hide")
-            rightColGastaldi.classList.remove("right-col-hide")
-        }, 1300);
-    } else if (subpage == "esperienze") {
-        var leftCol = document.getElementById("leftCol")
-        var rightCol = document.getElementById("rightCol")
-        var row = document.getElementById("row")
-
-        leftCol.classList.add("left-col-show")
-        rightCol.classList.add("right-col-show")
-        setTimeout(()=> {
-            setTimeout(() => {
-                leftCol.classList.remove("left-col-show")
-                rightCol.classList.remove("right-col-show")
-                leftCol.classList.add("left-col-hide")
-                rightCol.classList.add("right-col-hide")
-            }, 100);
-            row.classList.remove("text-show")
-            row.classList.add("text-hide")
-        }, 100)
-        setTimeout(() => {
-            leftCol.classList.remove("left-col-hide")
-            rightCol.classList.remove("right-col-hide")
-            row.classList.remove("text-hide")
-        }, 1300);
-    } else {
-        var leftCol = document.getElementById("leftCol")
-        var rightCol = document.getElementById("rightCol")
-        leftCol.classList.add("left-col-show")
-        rightCol.classList.add("right-col-show")
-        setTimeout(()=> {
-            leftCol.classList.remove("left-col-show")
-            rightCol.classList.remove("right-col-show")
-            leftCol.classList.add("left-col-hide")
-            rightCol.classList.add("right-col-hide")
-        }, 100)
-        setTimeout(() => {
-            leftCol.classList.remove("left-col-hide")
-            rightCol.classList.remove("right-col-hide")
-        }, 1300);
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////
 /// Funzione per mostrare le colonne e il testo (utilizzata per le animazioni
 /// delle pagine)
 ///////////////////////////////////////////////////////////////////////////////
@@ -112,7 +31,7 @@ export const showCols = () => {
     // se la pagina è "educazione" o "esperienze" viene eseguita una 
     // animazione diversa di apertura delle colonne e del testo, altrimenti 
     // viene eseguita una animazione di chiusura standard
-    if (subpage == "educazione") {
+    if (subpage == "/educazione") {
         var firstRow = document.getElementById("firstRow")
         var secondRow = document.getElementById("secondRow")
         var leftCol = document.getElementById("leftCol")
@@ -141,7 +60,7 @@ export const showCols = () => {
             leftCol.classList.remove("col-show-anim")
             rightCol.classList.remove("col-show-anim")
         }, 1300);
-    } else if (subpage == "esperienze") {
+    } else if (subpage == "/esperienze") {
         var leftCol = document.getElementById("leftCol")
         var rightCol = document.getElementById("rightCol")
         var row = document.getElementById("row")
